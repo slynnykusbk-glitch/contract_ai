473490aa99d9aac23c5c1f217921feb3bf092cb0
slynnykusbk-glitch <s.lynnyk.usbk@gmail.com>
2025-09-22 10:27:11 +0100

    Add structured entity extractors

--- contract_review_app/analysis/__init__.py
+++ contract_review_app/analysis/__init__.py
@@ -2,4 +2,26 @@
 
 from .labels_taxonomy import LABELS_CANON, resolve_labels
 
-__all__ = ["LABELS_CANON", "resolve_labels"]
+from .extractors import (
+    extract_amounts,
+    extract_dates,
+    extract_durations,
+    extract_incoterms,
+    extract_jurisdiction,
+    extract_law,
+    extract_percentages,
+    extract_roles,
+)
+
+__all__ = [
+    "LABELS_CANON",
+    "resolve_labels",
+    "extract_amounts",
+    "extract_percentages",
+    "extract_durations",
+    "extract_dates",
+    "extract_law",
+    "extract_jurisdiction",
+    "extract_incoterms",
+    "extract_roles",
+]
